c3312b53cfb1d6ad6d5d3ff5c41474e0402d7d74
KseniiaGWritesCode <gerasimenkokv1207@gmail.com>
2025-08-04 15:34:05 +0200

    Started with the program

---
+++ Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracking
+{
+    class Project : IToDoProperties
+    {
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        Priority Priority { get; set; }
+        Task Task { get; set; }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTracking
+{
+    class Task : IToDoProperties
+    {
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; }
+        Priority Priority { get; set; }
+        Project Project { get; set; }
+        Coworker Manager { get; set; }
+    }
+}
