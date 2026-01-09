dc3390c7de535978c6280120cf03d33cac4ec84d
dmg25 <gerasimenkodm25@gmail.com>
2026-01-09 15:22:04 +0100

    Started with ASP.Net...

---
+++ Task-tracking/Program.cs
@@ -0,0 +1,50 @@
+using NLog;
+using NLog.Web;
+using TaskTracking;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskTracking
+{
+    // Entity framework
+
+    // Controllers
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
+            logger.Info("Initialize app...");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            logger.Info($"Starting in Developer mode = {builder.Environment.IsDevelopment()}");
+
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(
+                    builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+
+            var app = builder.Build();
+
+
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            // Finish with EF. Add classes-models without DTO and stuff. Ensure that connected to DB and OLD ENTITIES ARE COMPATIBLE
+
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
